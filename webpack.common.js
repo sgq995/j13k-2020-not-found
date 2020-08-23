cba57a3ffa293d4d8c4571fd60465a10504a51a0
Sebastian G. Q <sgq995@gmail.com>
2020-08-23 12:38:53 -0500

    chore: Modified build trying to produce smallest bundles

--- webpack.common.js
+++ webpack.common.js
@@ -2,18 +2,39 @@ const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Not Found',
             template: './public/index.html',
         }),
+        // new HtmlWebpackInlineSourcePlugin({}),
+        new OptimizeCssAssetsPlugin({}),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
     },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        usedExports: true,
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                mangle: true,
+                compress: true,
+            },
+        })],
+    }
 });
